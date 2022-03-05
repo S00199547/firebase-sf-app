ca61a60a7b878335857d863f5fbc196c6ec6ad4b
Mahnoor-Akhter <60778404+Mahnoor-Akhter@users.noreply.github.com>
2022-03-05 22:23:19 +0000

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseApiService } from './firebase-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebase-sf-app';
+  MyBooks:any={};
+  titleValue='';
+  authorValue='';
+
+  constructor(public firebaseApiService: FirebaseApiService){
+
+  }
+
+  ngOnInit(): void {
+      this.loadBooks();
+  }
+  loadBooks(){
+    return this.firebaseApiService.getBooks().subscribe((data:{})=>
+    {
+this.MyBooks=data;
+    })
+    }
+
+    addBook(){
+      return this.firebaseApiService.addBook(this.titleValue,this.authorValue).subscribe((data:{})=>{
+        this.MyBooks=data;
+       this.titleValue='';
+    this.authorValue='';})
+    
+      
+
+}
+deleteBook(id:string){
+  return this.firebaseApiService.delBook(id).subscribe((data:{})=>{
+    this.MyBooks=data;
+  })
+}
+
 }
--- src/app/firebase-api.service.ts
+++ src/app/firebase-api.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
-
+import { Observable,throwError } from 'rxjs';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
+import { Book } from './book';
+import { retry,catchError} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseApiService {
+apiURL='';
+  constructor(private http: HttpClient) {}
+  
+  httpOptions={
+    headers:new HttpHeaders({
+      'Content-type':'application/jason'
+    })
+  }
 
-  constructor() { }
+  
+
+getBooks():Observable<Book>{
+  return this.http.get<Book>(this.apiURL +'/getBooks').pipe(retry(1),catchError(this.handleError))
+}
+
+handleError(error: { error: { message: string; }; status: any; message: any; }){
+  let errorMessage='';
+  if(error.error instanceof ErrorEvent){
+    errorMessage = error.error.message;
+  }
+  else{
+    errorMessage=`Error Code: ${error.status}\nMessage:${error.message}`;
+  }
+window.alert(errorMessage);
+return throwError(errorMessage);
+
+}
+addBook(title:string,author:string):Observable<Book>{
+  return this.http.post<Book>(this.apiURL+'/addBook?title=' +author,null).pipe(retry(1),catchError(this.handleError))
+}
+
+delBook(id:string):Observable<Book>{
+  return this.http.delete<Book>(this.apiURL+'/deleteBook?id=' +id).pipe(retry(1),catchError(this.handleError))
+}
+
 }
